99b2eb4f355942239cdf29f899cc669bbbdb1176
Chengwei Chou <s955401@gmail.com>
2019-04-09 11:29:36 +0800

    complete readInfo()

--- rmtorrent.py
+++ rmtorrent.py
@@ -3,13 +3,31 @@ import os
 
 class DelugeControl:
     def __init__(self):
-        pass
+        self.torrents = self.readInfo()
 
     def readInfo(self):
         '''
-        Parse Info to dictionart
+        Parse Info to dictionary
         '''
+
         os.system("deluge-console info > info.txt")
+
+        dict_content = {}
+        torrent_list = []
+        with open("info.txt") as f:
+            f.readline()
+            for line in f:
+                line = line.strip(" ")
+                if line == '\n':
+                    torrent_list.append(dict_content)
+                    dict_content = {}
+                    continue
+
+                sline = line.split(":")
+                dict_content[sline[0]] = sline[1].strip(' ').strip('\n')
+
+        os.system("rm info.txt")
+        return torrent_list
 
     def rmCompleteTorrent(self):
         pass
@@ -19,6 +37,7 @@ if __name__ == '__main__':
     print("test")
 
     dc = DelugeControl()
-    dc.readInfo()
+
+    print(dc.torrents)
 
     print("done")
